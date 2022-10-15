ffab6c62d9faff228a4582252a898357cdd74f0d
Samuel Marandi <samuelcmarandi@gmail.com>
2022-10-15 19:58:44 +0530

    modularise my server implementation

--- server.py
+++ server.py
@@ -1,21 +1,10 @@
 
-from http.server import (HTTPServer, BaseHTTPRequestHandler)
+from http.server import (HTTPServer)
+from http_handler import (MyServer)
+
 # import time
 hostName = "localhost"
 serverPort = 8888  # You can choose any available port; by default, it is 8000
-
-class MyServer(BaseHTTPRequestHandler):
-    print('Server up and running...')
-
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":
     webServer =  HTTPServer((hostName, serverPort), MyServer)
